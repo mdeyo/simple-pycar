e7ebd31db764bf242222ecd8c776f5ed6025bdfe
DEYO <mdeyo@mit.edu>
2017-03-20 07:58:21 -0400

    starting discrete grid game

---
+++ grid_game.py
@@ -0,0 +1,11 @@
+# Dumbed down version of car simulator
+# For first tests of Q-learning techniques
+
+# 5x5 grid world
+# Our car starts in one of the 10 blocks in the left-most 2 columns
+# There are two or three red squares with a reward of -10 (obstacles)
+# There is a region on the right-most side with reward 10
+# Our car can take one of 3 actions each turn:
+#   drive straight - car moves one square forward
+#   drive left
+#   drive right
